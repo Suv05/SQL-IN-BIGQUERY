b4c299d44d315423e5332870e7918e7daf7b3b10
suvam <suvampravu@gmail.com>
2024-10-25 01:10:18 +0530

    Commit changes

---
+++ Fanatasy Dataset/Day-1.sql
@@ -0,0 +1,16 @@
+-------------
+-- SELECT name,id
+
+-- FROM fantasy.Characters
+
+-------------
+
+-------------
+
+---aliasing (Rename the coloumn)
+
+SELECT name,id,level AS char_level
+
+FROM fantasy.Characters
+
+-------------
