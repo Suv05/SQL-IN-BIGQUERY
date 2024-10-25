bd9f888a203d188f6ee56243959c01cb3489a85b
suvam <suvampravu@gmail.com>
2024-10-26 02:57:28 +0530

    Commit changes

--- Fanatasy Dataset/Day-6.sql
+++ Fanatasy Dataset/Day-6.sql
@@ -5,3 +5,21 @@ WHERE is_alive=true
 ORDER BY level
 LIMIT 5
 
+---bracketing in sql
+
+-- up to lvl-15 low
+-- up to lvl-15 to 25 mid
+-- more than lvl-25 super
+
+SELECT name,level, level>=20 AS level_goat,
+
+CASE
+  when level<15 then "low"
+  when level<=25 then "mid"
+  else "super"
+
+END AS level_bucket
+
+FROM fantasy.Characters
+WHERE is_alive=true
+
