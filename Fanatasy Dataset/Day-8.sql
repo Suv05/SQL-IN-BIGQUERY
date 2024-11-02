e133015003a9c3dab58b64fdf064561036705025
suvam <suvampravu@gmail.com>
2024-11-02 19:28:09 +0530

    Commit changes

---
+++ Fanatasy Dataset/Day-8.sql
@@ -0,0 +1,27 @@
+-- find the difference between a chracter's experince and their mentor's
+--corelated subquires
+SELECT id AS mentee_id, mentor_id,
+(
+  SELECT experience
+  FROM fantasy.Characters AS MENTOR_TABLE
+  WHERE id=MENTEE_TABLE.mentor_id
+) - experience AS experience_difference
+FROM fantasy.Characters AS Mentee_Table
+WHERE mentor_id IS NOT NULL;
+
+
+
+
+--uncorelated subquires
+SELECT name,experience
+FROM `fantasy.Characters`
+WHERE experience > (
+  --2100
+  SELECT MIN(experience)
+  FROM `fantasy.Characters`
+)
+AND experience <(
+  --15000
+  SELECT MAX(experience)
+  FROM `fantasy.Characters`
+)
