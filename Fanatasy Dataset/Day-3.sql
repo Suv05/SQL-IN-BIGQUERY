fd5f1b1ec7a2872793c1d2a300acc0708aeeac30
suvam <suvampravu@gmail.com>
2025-02-03 23:37:53 +0530

    Commit changes

--- Fanatasy Dataset/Day-3.sql
+++ Fanatasy Dataset/Day-3.sql
@@ -14,6 +14,22 @@ AS(
   WHERE is_alive=false
 );
 
+---TO AVOID ALREADY EXITS TABLE ERROR
+---SOLUTION-1
+CREATE TABLE OR REPLACE fantasy.Characters_Dead
+AS(
+  SELECT *
+  FROM fantasy.Characters
+  WHERE is_alive=false
+);
+---SOLUTION-2
+CREATE TABLE IF NOT EXISTS fantasy.Characters_Dead
+AS(
+  SELECT *
+  FROM fantasy.Characters
+  WHERE is_alive=false
+);
+
 ---merge result these two tables by UNION OPERATOR and Intersect operation
 SELECT * FROM fantasy.Characters_Alive
 
