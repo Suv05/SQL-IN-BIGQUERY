54ecf8140648b6ac81174be4f37960e7f1a93b26
suvam <suvampravu@gmail.com>
2025-02-05 12:58:06 +0530

    Commit changes

--- Fanatasy Dataset/Day-11.sql
+++ Fanatasy Dataset/Day-11.sql
@@ -1,5 +1,18 @@
 --join two table
-SELECT *
-FROM fantasy.characters
-JOIN fantasy.inventory
-on inventory.character_id=characters.id
+SELECT
+    *
+FROM
+    fantasy.characters
+    JOIN fantasy.inventory on inventory.character_id = characters.id
+
+--a character can use any item for which the power level
+--is equal or greater than the character's experience
+--divided by 100
+
+--a list of all characters and the items that they can use
+
+SELECT c.name, c.experience/100, i.name, i.power
+FROM fantasy.characters c
+JOIN fantasy.items i 
+on c.experience/100 <= i.power OR c.class= "Mage"
+ORDER BY c.name
