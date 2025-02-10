2d66d76fbce7cc3452c2d07261de8e2bd2a2c986
suvam <suvampravu@gmail.com>
2025-02-11 00:08:38 +0530

    Commit changes

--- Fanatasy Dataset/Day-16.sql
+++ Fanatasy Dataset/Day-16.sql
@@ -1,1 +1,37 @@
 --window function
+SELECT
+    name,
+    item_type,
+    'power',
+    SUM(power) OVER () AS sum_power
+FROM
+    fantasy.items
+
+
+-- show, for each item, what % of total power it covers
+SELECT
+    name,
+    item_type,
+    'power',
+    SUM('power') OVER () AS sum_power,
+    'power' / SUM('power') OVER () * 100 AS percent_total_power,
+    AVG("power") OVER () AS avg_power
+FROM
+    fantasy.items
+
+
+
+-- -- --- ---- ----- -----
+SELECT name, item_type, power,
+SUM(power) OVER(PARTITION BY item_type)
+FROM fantasy.items
+
+SELECT name, item_type, power,
+SUM(power) OVER(ORDER BY power ASC)
+FROM fantasy.items
+
+-- -- --- ---- ----- -----
+SELECT name, item_type, power,
+SUM(power) OVER(PARTITION BY item_type ORDER BY power ASC)
+FROM fantasy.items
+ORDER BY item_type, power
